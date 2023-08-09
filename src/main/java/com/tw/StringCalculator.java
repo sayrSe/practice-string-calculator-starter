b01079c0085c9859ea848a646a724f25d39a678f
Jose Angelo Arboleda <joseangelo.arboleda@gmail.com>
2023-08-09 16:19:28 +0800

    (Java Exam): Modify add function in StringCalculator

--- src/main/java/com/tw/StringCalculator.java
+++ src/main/java/com/tw/StringCalculator.java
@@ -1,7 +1,19 @@
 package com.tw;
 
+import java.util.Arrays;
+
 public class StringCalculator {
+
     public int add(String string) {
-        return 0;
+        if (!string.contains(",") && !string.contains("\n")) {
+            return string.isEmpty() ? 0 : Integer.parseInt(string);
+        }
+
+        String[] lines = string.split("[,\n]");
+
+        return lines[0].contains("//") ?
+                Arrays.stream(lines[1].split(lines[0].substring(2)))
+                        .mapToInt(Integer::parseInt).sum() :
+                Arrays.stream(lines).mapToInt(Integer::parseInt).sum();
     }
 }
